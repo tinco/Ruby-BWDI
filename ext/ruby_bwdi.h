5156541821f3fd85b91c7f88d90b260d8ad3919f
Tinco Andringa <mail@tinco.nl>
2011-03-12 20:43:38 +0100

    start on abstract member getter

--- ext/ruby_bwdi.h
+++ ext/ruby_bwdi.h
@@ -12,3 +12,17 @@
 #include <BWDI.h>
 
 typedef VALUE(*RubyFunction)(ANYARGS);
+
+template<T C::*M, VALUE (*PREPARE)(T), typename T, typename C>
+VALUE getMember(VALUE self) {
+  C * us;
+  Data_Get_Struct(self, C, us);
+  return PREPARE(us->*M);
+}
+
+#define DEFINE_GETTER(THIS, FUNCNAME, MEMBER, PREPARE) \
+if(false) \
+{ \
+getMember<&MEMBER, PREPARE>(0); \
+} \
+rb_define_method(THIS, FUNCNAME, (RubyFunction)getMember<&MEMBER, PREPARE>, 0);
